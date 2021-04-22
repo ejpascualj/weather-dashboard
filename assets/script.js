20c54f978602e58c6c75581dd190acd71e4685c5
ejpascualj <ejpascualj@gmail.com>
2021-04-22 04:43:23 -0500

    corrected local storage + added bootstrap and css

--- assets/script.js
+++ assets/script.js
@@ -26,11 +26,8 @@ var ForecastDay3El = document.querySelector("#ForecastDay3");
 var ForecastDay4El = document.querySelector("#ForecastDay4");
 
 // FUNCTIONS
-
-
 // 1. Function that fetches weather API based on city
 function FetchWeather(City) {
-    
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + City + "&appid=4160ec3520c203fccfead3fd09fe42ff")
         .then(function (response) {
             return response.json();
@@ -38,8 +35,8 @@ function FetchWeather(City) {
             console.log(data);
             // Populate Title Data
             CityNameEl.textContent = City;
-            DateEl.textContent = data.dt;
-            IconEl.textContent = data.weather[0].icon;
+            DateEl.textContent = moment(data.dt, "X").format("DD-MMM-YYYY");
+            IconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
             // Populate Current Weather Data
             TempEl.textContent = data.main.temp;
             WindEl.textContent = data.wind.speed;
@@ -58,22 +55,23 @@ function FetchWeather(City) {
             //5-day forecast
             for (var i = 1; i < 6; i++) {
                 //select variables to be displayed
-                var ForecastDate = data02.daily[i].dt;
+                var ForecastDate = moment(data02.daily[i].dt, "X").format("DD-MMM-YYYY");
                 var ForecastIcon = data02.daily[i].weather[0].icon;
                 var ForecastTemp = data02.daily[i].temp.max;
                 var ForecastWind = data02.daily[i].wind_speed;
                 var ForecastHumidity = data02.daily[i].humidity;
                 // create a new element within Forecast container
                 var ForecastList = document.createElement("ul");
+                ForecastList.classList = "col border";
                 ForecastContainerEl.appendChild(ForecastList);
 
                 var ForecastDateEl = document.createElement("li");
                 ForecastDateEl.textContent = ForecastDate
                 ForecastList.appendChild(ForecastDateEl)
-
-                var ForecastIconEl = document.createElement("li");
-                ForecastIconEl.textContent = ForecastIcon
-                ForecastList.appendChild(ForecastIconEl)
+                
+                var ForecastIconImg = document.createElement("img");
+                ForecastIconImg.setAttribute("src", "http://openweathermap.org/img/wn/" + ForecastIcon + ".png");
+                ForecastList.appendChild(ForecastIconImg)
 
                 var ForecastTempEl = document.createElement("li");
                 ForecastTempEl.textContent = ForecastTemp
@@ -88,7 +86,6 @@ function FetchWeather(City) {
                 ForecastList.appendChild(ForecastHumidityEl)
             }
         })
-    // AddCityButton(City)
 }
 
 // 3. Function to populate Search History Buttons with latest searches (unshift 0th element, pop 7th element)
@@ -100,30 +97,30 @@ function AddCityButton(City) {
 }
 
 // 4. Store and Load Search History from Local Storage
-function LocalStorageStore(){
+function LocalStorageStore() {
     var City = CityInput.value.trim().toUpperCase();
     SearchHistory.push(City)
     localStorage.setItem("search", JSON.stringify(SearchHistory));
     // var SearchHistory = JSON.parse(localStorage.getItem("Cities"));
     // SearchHistory.push(City);
-    CityInput.value="";
+    CityInput.value = "";
     // function to clear fetched weather data
     ClearData();
 }
 
-function LocalStorageLoad(){
-    if (localStorage.getItem("search")){
+function LocalStorageLoad() {
+    if (localStorage.getItem("search")) {
         var StoredCities = JSON.parse(localStorage.getItem("search"))
         console.log(StoredCities);
-        for (var i =0; i<StoredCities.length; i++){
+        for (var i = 0; i < StoredCities.length; i++) {
             AddCityButton(StoredCities[i]);
         }
     }
-    
+
     var CityButtons = document.getElementsByClassName("CityButton");
     console.log(CityButtons)
-    for (i=0; i<CityButtons.length; i++){
-        CityButtons[i].addEventListener("click",function(){
+    for (i = 0; i < CityButtons.length; i++) {
+        CityButtons[i].addEventListener("click", function () {
             City03 = this.innerText
             console.log(City03)
             FetchWeather(City03);
@@ -132,21 +129,12 @@ function LocalStorageLoad(){
     }
 }
 
-function ClearData(){
+function ClearData() {
     ForecastContainerEl.innerHTML = "";
 }
 
-// // 0. Auxiliary Function to trigger all functions
-// function Search(event){
-//     event.preventDefault();
-//     var City = CityInput.value.trim().toUpperCase();
-//     FetchWeather(City);
-//     AddCityButton(City);
-//     LocalStorageStore();
-// }
-
 // EVENT LISTENERS
-SubmitButton.addEventListener("click", function(event){
+SubmitButton.addEventListener("click", function (event) {
     event.preventDefault();
     var City = CityInput.value.trim().toUpperCase();
     FetchWeather(City);
